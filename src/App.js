4558e3c991419fa4714d574da643549bf4784680
siddharthsharma96 <sid9nov@gmail.com>
2023-06-11 21:11:28 +0530

    Added subheader

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 // import ListItem from "./components/Listitem/ListItems";
 import Product from "./components/Product.js";
 import Header from "./components/Layout/Header";
+import SubHeader from "./components/Layout/SubHeader";
 function App() {
   return (
     <div>
       <Header></Header>
+      <SubHeader></SubHeader>
       <Product></Product>
     </div>
   );
---
+++ src/components/Layout/SubHeader.js
@@ -0,0 +1,34 @@
+const SubHeader = () => {
+  return (
+    <div className="subHeader">
+      <ul className="subHeader-List">
+        <li className="subheader-Listitem Active">
+          {/* <a className="subHeader-links" href=""> */}
+          Home
+          {/* </a> */}
+        </li>
+        <li className="subheader-Listitem ">
+          {/* <a className="subHeader-links" href="#"> */}
+          Product
+          {/* </a> */}
+        </li>
+        <li className="subheader-Listitem ">
+          {/* <a className="subHeader-links" href="#"> */}
+          Contact Us
+          {/* </a> */}
+        </li>
+        <li className="subheader-Listitem ">
+          {/* <a className="subHeader-links" href="#"> */}
+          About
+          {/* </a> */}
+        </li>
+        <li className="subheader-Listitem ">
+          {/* <a className="subHeader-links" href="#"> */}
+          Info
+          {/* </a> */}
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default SubHeader;
